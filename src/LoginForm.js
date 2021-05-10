cdb7b1454d385cb807632ba773b817d1f877135c
Springer Jasmin-Joy <js409@hdm-stuttgart.de>
2021-05-10 19:58:08 +0200

    Add Loginform

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,11 +1,44 @@
 import React from 'react'
 
-const LoginForm = () => {
-    return (
-        <div>
+export default class LoginForm extends React.Component {
+    constructor(props) {
+        super(props)
 
-        </div>
-    )
+        this.state = {
+            username: "",
+            password: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target
+        const value = target.value
+        const name = target.name
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit(event) {
+        console.log("Submit")
+        event.preventDefault()
+    }
+    
+    render() {
+        return (
+        <form id="login-form" onSubmit={this.handleSubmit}>
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" placeholder="Enter username..." value={this.state.username} onChange={this.handleChange} required />
+
+            <label htmlFor="password">Passwort</label>
+            <input type="password" name="password" placeholder="Enter password..." value={this.state.password} onChange={this.handleChange} required />
+
+            <button type="submit">Submit</button>
+        </form>
+        )
+    }
 }
-
-export default LoginForm
