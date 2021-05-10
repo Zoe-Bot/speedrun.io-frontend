0247511327d6f37ad044403f5756e7202e81a50d
Springer Jasmin-Joy <js409@hdm-stuttgart.de>
2021-05-10 16:59:27 +0200

    Covert to Jsx

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import Pet from './Pet'
 
 
 const App = () => {
-    return React.createElement(
-        "div",
-        { id: "important" },
-        [React.createElement("h1", {}, "Hello"),
-        React.createElement(Pet, { name: "Luna", animal: "cat", breed: "havanise" }),
-        React.createElement(Pet, { name: "Haha", animal: "bird", breed: "havanise" }),
-        React.createElement(Pet, { name: "Pepper", animal: "cat", breed: "havanise" })
-        ]
+    // return React.createElement(
+    //     "div",
+    //     { id: "important" },
+    //     [React.createElement("h1", {}, "Hello"),
+    //     React.createElement(Pet, { name: "Luna", animal: "cat", breed: "havanise" }),
+    //     React.createElement(Pet, { name: "Haha", animal: "bird", breed: "havanise" }),
+    //     React.createElement(Pet, { name: "Pepper", animal: "cat", breed: "havanise" })
+    //     ]
+    // )
+    return (
+        <div>
+            <h1 id="Hello">Adopt me!</h1>
+            <Pet name="Luna" animal="Dog" breed="Havanese" />
+            <Pet name="Pepper" animal="Bird" breed="Havanese" />
+            <Pet name="Doink" animal="Cat" breed="Havanese" />
+        </div>
     )
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"))
+render(<App />, document.getElementById("root"))
