55581e906332d7c7770e8883bbd53d54c9359a49
Springer Jasmin-Joy <js409@hdm-stuttgart.de>
2021-05-11 21:46:04 +0200

    Add display all maps

--- src/components/MapList.js
+++ src/components/MapList.js
@@ -1,34 +1,49 @@
-import React from "react"
+import React, { useEffect } from "react"
+import useState from 'react-usestateref'
 import { Link } from 'react-router-dom'
 
-const MapList = ({ maps }) => (
-    <table>
-        <thead>
-            <tr>
-                <th>Mapname</th>
-                <th>Author</th>
-                <th>Difficulty</th>
-            </tr>
-        </thead>
-      
-        <tbody>
-            {maps.map((map, key) => (
-                <tr key={key} >
-                    <td>
-                        <Link to={`/map/${map.id}`}>
-                            {map.name}
-                        </Link>
-                    </td>
-                    <td>
-                        {map.authorId}
-                    </td>
-                    <td>
-                        {map.difficulty}
-                    </td>
+const MapList = () => {
+    const [maps, setMaps, mapsRef] = useState()
+
+    useEffect(() => {
+        const fetchMaps = async () => {
+            const result = await fetch('http://localhost:3000/v1/maps')
+            const body = await result.json()
+            setMaps(body)
+            console.log(mapsRef)
+        }
+        fetchMaps()
+    }, [])
+    
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Mapname</th>
+                    <th>Author</th>
+                    <th>Difficulty</th>
                 </tr>
-            ))}
-        </tbody>
-    </table>
-)
+            </thead>
+
+            <tbody>
+                {maps && maps.map((map, key) => (
+                    <tr key={key} >
+                        <td>
+                            <Link to={`/map/${map._id}`}>
+                                {map.name}
+                            </Link>
+                        </td>
+                        <td>
+                            {map.authorId}
+                        </td>
+                        <td>
+                            {map.difficulty}
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
 
 export default MapList
