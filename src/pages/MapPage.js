ae063897e3c54fa02712fcf979edcf800cb5d613
Springer Jasmin-Joy <js409@hdm-stuttgart.de>
2021-05-11 21:46:17 +0200

    Refactor map fetch

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -4,47 +4,29 @@ import ErrorPage from './ErrorPage'
 
 const MapPage = ({ match }) => {
     const id = match.params.id
-    //609a96cbcb6c3d6b9008a0b3
-
     const [map, setMap, mapRef] = useState()
-    const [user, setUser, userRef] = useState()
-    const [highscores, setHighscores, highscoresRef] = useState()
 
     useEffect(() => {
-        const fetchMaps = async () => {
-            const result = await fetch(`http://localhost:3000/v1/maps/${id}`)
-            const body = await result.json();
-            setMap(body)
-            fetchUser()
+        const fetchMap = async () => {
+            const result = await fetch(`http://localhost:3000/v1/maps/frontend/${id}`)
+            const body = await result.json()
+            setMap(body[0])
         }
-        fetchMaps()
-
-        const fetchUser = async () => {
-            const result = await fetch(`http://localhost:3000/v1/users/${mapRef.current.authorId}`)
-            const body = await result.json();
-            setUser(body)
-        }
-
-        const fetchHighscores = async () => {
-            const result = await fetch(`http://localhost:3000/v1/maps/${id}/highscores`)
-            const body = await result.json();
-            setHighscores(body)
-            console.log(highscoresRef)
-        }
-        fetchHighscores()
+        fetchMap()
     }, [id])
 
+    console.log(mapRef.current)
     return (
         <div>
-            { map && user && highscores && 
+            { map && 
             <>
                 <h1>{map.id} {map.name}</h1>
-                <p>From: {user.username}</p>
+                <p>From: {map.author.username}</p>
                 <p>difficulty: {map.difficulty}</p>
                 <h2>Tags</h2>
                 {map.tags.map((tag, key) => <p key={key}>{tag}</p>)}
                 <h2>Highscores</h2>
-                {highscores.map((highscore, key) => <p key={key}>{highscore.score}</p>)}
+                
             </>
             }
         </div>
