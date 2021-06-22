7007de3bb5d2479fe52a50be923a64a8f6eebddb
Springer Jasmin-Joy <js409@hdm-stuttgart.de>
2021-06-22 10:57:24 +0200

    Fixed issue token stored with ""

--- src/customHooks/useToken.js
+++ src/customHooks/useToken.js
@@ -3,14 +3,14 @@ import { useState } from 'react'
 const useToken = () => {
     const getToken = () => {
         const tokenString = localStorage.getItem('auth_token')
-        const userToken = JSON.parse(tokenString)
+        const userToken = tokenString
         return userToken
     }
     
     const [token, setToken] = useState(getToken())
 
     const saveToken = (userToken) => {
-        localStorage.setItem('auth_token', JSON.stringify(userToken.access_token))
+        localStorage.setItem('auth_token', userToken.access_token)
         setToken(userToken.access_token)
     }
 
