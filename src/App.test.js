e93a5f82d51744fc4e89f1821c5c637b4d855543
Lars Flieger <a.purenetwork@gmail.com>
2021-06-30 21:39:54 +0200

    Update package and add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders play now link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Play now/i);
   expect(linkElement).toBeInTheDocument();
 });
