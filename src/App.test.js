77838718535f7ff531b1937fc1711141accdebda
Lars Flieger <a.purenetwork@gmail.com>
2021-07-07 20:34:41 +0200

    Update test example

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, getByPlaceholderText, getByText, render, screen } from '@testing-library/react';
 import App from './App';
+// https://testing-library.com/docs/react-testing-library/cheatsheet
 
-test('renders play now link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Play now/i);
-  expect(linkElement).toBeInTheDocument();
-});
+beforeEach(() => {
+	render(<App />)
+})
+
+test('should display landing page', () => {
+	const linkElement = screen.getByText(/Play now/i)
+	expect(linkElement).toBeInTheDocument()
+})
+
+test('should switch to login page', async () => {
+	fireEvent.click(screen.getByText(/Login/i))
+	const loginHeadline = await screen.findByText(/Welcome back/i)
+	expect(loginHeadline).toHaveClass("fst-italic")
+})
+
+test('should fail login', async () => {
+		// Enters data into input vield
+		fireEvent.change(screen.getByPlaceholderText(/Username/i), { target: { value: 'Username' } })
+		fireEvent.change(screen.getByPlaceholderText(/Password.../i), { target: { value: '1234' } })
+		
+		fireEvent.click(screen.getByText(/Sign in/i))
+		const errorMessage = await screen.findByText(/Wrong username or password!/i)
+		expect(errorMessage).toHaveClass("invalid-feedback")	
+})
